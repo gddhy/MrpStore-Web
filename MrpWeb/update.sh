9c3a6d4aae072e935058e2e8a87bfe43546db994
gddhy <gddhywgy@gmail.com>
2023-03-30 08:49:50 +0800

    调整构建

--- MrpWeb/update.sh
+++ MrpWeb/update.sh
@@ -2,6 +2,7 @@
 echo "脚本开始执行"
 echo "update.sh" >> .gitignore
 echo "log.txt" >> .gitignore
+echo "mrp.gddhy.net" > CNAME
 str=$(date "+%Y-%m-%d %H:%M:%S")
 info="update: ${str} (UTC+8)"
 echo $info
---
+++ cf_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "cf bulid start"
+java -jar MrpStoreWeb.jar
+rm -f MrpWeb/pdate.sh
